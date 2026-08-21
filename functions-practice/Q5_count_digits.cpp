458cbe8b86d9f57846a3e0e12c7106c4336d650a
Sujal Giri <250813995+sujalgiriiitp-source@users.noreply.github.com>
2026-08-21 19:59:06 +0530

    Add functions practice Q5 solution

---
+++ functions-practice/Q5_count_digits.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int countDigits(int n) {
+    int count = 0;
+
+    while (n != 0) {
+        n = n / 10;
+        count++;
+    }
+
+    return count;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int result = countDigits(n);
+    cout << "Number of digits = " << result << endl;
+
+    return 0;
+}
